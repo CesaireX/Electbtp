cae137da11cb84a3e05c1590e365ed194380cbe7
CesaireX <cesairetoe@gmail.com>
2022-07-27 19:25:23 +0000

    okk4

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-//Install express server
+
 let express = require('express');
 
 let app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/elect-btp-project'));
 
 app.get('/*',(req,res)=>{
-res.sendFile(__dirname+'/dist/elect-btp-project/index.html');
+   res.sendFile(__dirname+'/dist/elect-btp-project/index.html');
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
